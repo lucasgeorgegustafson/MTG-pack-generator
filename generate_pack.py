91cf531cac98c77cdb0eecd0dfa16f541796307a
Ian Gustafson <ian@dispatch.me>
2021-03-25 00:26:24 -0400

    Fix handling of mythics
    * Mythics replace rares 12.5% of the time
    * Make expansion code case-insensitive

--- generate_pack.py
+++ generate_pack.py
@@ -2,39 +2,50 @@ import json
 import sys
 import random
 
-def generate_pack(filepath, expansion, num_packs=1):
+def generate_pack(filepath, expansion):
 
     with open(filepath) as cards_json:
-
         cards = json.load(cards_json)
 
     set_cards = []
     commons = []
     uncommons = []
-    rares_and_mythics = []
+    rares = []
+    mythics = []
     pack = []
 
     LANDS = ['Swamp', 'Island', 'Plains', 'Mountain', 'Forest']
 
     for card in cards:
-
-        if card['set'] == expansion and 'Basic' not in card['type_line']:
+        if card['set'] == expansion.lower() and 'Basic' not in card['type_line']:
             set_cards.append(card)
 
     for card in set_cards:
-
         if card['rarity'] == 'common':
             commons.append(card)
         elif card['rarity'] == 'uncommon':
             uncommons.append(card)
+        elif card['rarity'] == 'rare':
+            rares.append(card)
+        elif card['rarity'] == 'mythic':
+            mythics.append(card)
         else:
-            rares_and_mythics.append(card)
+            raise Exception('Unknown card rarity"' + card['rarity'] + '" for card "' + card['name'] + '"')
+
+    number_of_mythics = 0
+    number_of_rares = 1
+    number_of_uncommons = 3
+    number_of_commons = 10
+
+    if roll_for_mythic() and len(mythics) > 0:
+        number_of_mythics, number_of_rares = 1, 0
 
     while num_packs > 0:
+        pack.extend([card['name'] for card in random.choices(mythics, k=number_of_mythics)])
+        pack.extend([card['name'] for card in random.choices(rares, k=number_of_rares)])
+        pack.extend([card['name'] for card in random.choices(uncommons, k=number_of_uncommons)])
+        pack.extend([card['name'] for card in random.choices(commons, k=number_of_commons)])
 
-        pack.append(random.choice(rares_and_mythics)['name'])
-        pack.extend([card['name'] for card in random.choices(uncommons, k=3)])
-        pack.extend([card['name'] for card in random.choices(commons, k=10)])
         pack.append(random.choice(LANDS))
 
         for card in pack:
@@ -44,6 +55,10 @@ def generate_pack(filepath, expansion, num_packs=1):
 
         num_packs -= 1
         pack = []
+
+def roll_for_mythic():
+    """Randomly check if a pack should have a mythic instead of a rare (12.5%)"""
+    return random.randint(1,1000) <= 125
 
 if __name__ == '__main__':
     try:

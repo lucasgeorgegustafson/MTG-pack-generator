495df761f8163373268793cb74e274c42582fe3a
Lucas Gustafson <lucasgustafun@gmail.com>
2021-05-01 11:09:53 -0400

    tie up loose ends in run_generator's early exit scheme

--- run_generator.py
+++ run_generator.py
@@ -3,26 +3,26 @@ import sys
 from set_dict import set_dict
 
 def run_generator(filepath, expansion, num_packs=1):
+    """
+    gen's initialization method is passed an empty
+    set for its set_cards parameter, which we can
+    then reassign after reading and parsing the bulk
+    data. This way, if we try to instantiate a generator
+    class which doesn't conform to our interface, the
+    program will exit without needlessly constructing
+    the set_cards list.
+    """
 
-    # gen's initialization method is passed an empty
-    # set for its set_cards parameter, which we can
-    # then reassign after reading and parsing the bulk
-    # data. This way, if we try to instantiate a generator
-    # class which doesn't conform to our interface, the
-    # program will exit without needlessly constructing
-    # the set_cards list.
-    gen = set_dict[expansion]([])
-
-    if expansion in set_dict:
+    if expansion not in set_dict:
+        print(f'Expansion {expansion} is unsupported')
+    else:
+        gen = set_dict[expansion]([])
         set_cards = get_set_cards(filepath, expansion)
         gen.set_cards = set_cards
 
         while num_packs > 0:
             gen.generate_pack()
             num_packs -= 1
-
-    else:
-        print(f'Expansion {expansion} is unsupported.')
 
 def get_set_cards(filepath, expansion):
 
